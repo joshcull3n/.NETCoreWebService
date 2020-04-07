87ca36e4feda5c813c6fd5e0d9bf5692891b8a80
murakilo <joshuacullen1993@gmail.com>
2020-04-06 21:32:24 -0700

    update Age property from string to int in studentregistrationreply.cs

--- StudentRegistrationDemo3/Models/StudentRegistrationReply.cs
+++ StudentRegistrationDemo3/Models/StudentRegistrationReply.cs
@@ -8,7 +8,7 @@ namespace StudentRegistrationDemo3.Models
     public class StudentRegistrationReply
     {
         public string Name { get; set; }
-        public string Age { get; set; }
+        public int Age { get; set; }
         public string RegistrationNumber { get; set; }
         public string RegistrationStatus { get; set; }
     }
