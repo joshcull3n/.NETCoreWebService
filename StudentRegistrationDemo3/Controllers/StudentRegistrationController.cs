8e9f52cc7d08f6530540ad8eaa35aa4a27349b0e
murakilo <joshuacullen1993@gmail.com>
2020-04-06 21:01:28 -0700

    add GET/POST controllers that return/insert student info

---
+++ StudentRegistrationDemo3/Controllers/StudentRegistrationController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+using StudentRegistrationDemo3.Models;
+
+namespace StudentRegistrationDemo3.Controllers
+{
+    [Route("api/[controller]")]
+    public class StudentRegistrationController : Controller
+    {
+        // POST: api/<controller>
+        [HttpPost]
+        public StudentRegistrationReply RegisterStudent(Student stuReg)
+        {
+            Console.WriteLine("In RegisterStudent()");
+            StudentRegistrationReply StuRegReply = new StudentRegistrationReply();
+            StudentRegistration.GetInstance().Add(stuReg);
+            StuRegReply.Name = stuReg.Name;
+            StuRegReply.Age = stuReg.Age;
+            StuRegReply.RegistrationNumber = stuReg.RegistrationNumber;
+            StuRegReply.RegistrationStatus = "Successful";
+
+            return StuRegReply;
+        }
+
+        [HttpPost("InsertStudent")]
+        public IActionResult InsertStudent(Student stuReg)
+        {
+            return Ok(RegisterStudent(stuReg));
+        }
+
+        [Route("student/")]
+        [HttpPost("AddStudent")]
+        public JsonResult AddStudent(Student stuReg)
+        {
+            return Json(RegisterStudent(stuReg));
+        }
+
+    }
+}   
---
+++ StudentRegistrationDemo3/Controllers/StudentRetrieveController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+using StudentRegistrationDemo3.Models;
+
+namespace StudentRegistrationDemo3.Controllers
+{
+    [Route("api/[controller]")]
+    public class StudentRetrieveController : Controller
+    {
+        // GET: api/controller
+        [HttpGet]
+        public List<Student> GetAllStudents()
+        {
+            return StudentRegistration.GetInstance().GetAllStudents();
+        }
+
+        [HttpGet("GetAllStudentRecords")]
+        public JsonResult GetAllStudentRecords()
+        {
+            return Json(GetAllStudents());
+        }
+    }
+}
